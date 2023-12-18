d90a0ce79fc67caf1df99b03d4636598714fc37e
Daven <davenlu1989@gmail.com>
2023-12-18 13:29:16 +0900

    fmt infra, first draft of readme

--- tf-resources/lambda_functions.tf
+++ tf-resources/lambda_functions.tf
@@ -2,36 +2,46 @@
 # create_sam_metadata = true is required to connect them
 
 module "lambda_function_tts" {
-  source        = "terraform-aws-modules/lambda/aws"
-  version       = "~> 6.0"
-  timeout       = 300
-  source_path   = var.source_code_path
-  function_name = "tts"
-  handler       = "app.lambda_handler"
-  runtime       = "python3.9"
+  source              = "terraform-aws-modules/lambda/aws"
+  version             = "~> 6.0"
+  timeout             = 300
+  source_path         = var.source_code_path
+  function_name       = "tts"
+  handler             = "app.lambda_handler"
+  runtime             = "python3.9"
   create_sam_metadata = true
-  publish       = true
-  architectures = [ "arm64" ]
+  publish             = true
+  architectures       = ["arm64"]
   environment_variables = {
-    "OPENAI_API_KEY" = var.openai_api_key
+    OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.secret_credentials.secret_string)["OPENAI_API_KEY"]
     S3_BUCKET_NAME = var.s3_bucket_name
   }
-  layers = [ aws_lambda_layer_version.lambda_openai_layer.arn ]
+  layers = [aws_lambda_layer_version.lambda_openai_layer.arn]
   allowed_triggers = {
     APIGatewayAny = {
       service    = "apigateway"
       source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
     }
   }
-  policy = aws_iam_policy.lambda_s3_policy.arn
+  policy        = aws_iam_policy.lambda_s3_policy.arn
   attach_policy = true
+}
+
+# Access openai API Key from secrets manager
+data "aws_secretsmanager_secret" "openai_api_key" {
+  name = var.secrets_manager_openai_api_secret_name
+}
+
+data "aws_secretsmanager_secret_version" "secret_credentials" {
+  secret_id = data.aws_secretsmanager_secret.openai_api_key.id
+
 }
 
 # AWS lambda module internally allows cw logs, so we manually attach an s3 policy 
 resource "aws_iam_policy" "lambda_s3_policy" {
   name = "audio_artifacts_lambda_s3_policy"
 
-    policy = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
@@ -51,9 +61,9 @@ resource "aws_iam_policy" "lambda_s3_policy" {
 
 # Lambda layer created from arm64 image
 resource "aws_lambda_layer_version" "lambda_openai_layer" {
-  description = "Lambda layer for Python 3.9, contains only openai and its dependencies"
-  filename   = var.openai_layer_abs_path
-  layer_name = "lambda-openai-layer"
-  compatible_architectures = [ "arm64" ]
-  compatible_runtimes = ["python3.9"]
+  description              = "Lambda layer for Python 3.9, contains only openai and its dependencies"
+  filename                 = var.openai_layer_abs_path
+  layer_name               = "lambda-openai-layer"
+  compatible_architectures = ["arm64"]
+  compatible_runtimes      = ["python3.9"]
 }
--- tf-resources/s3.tf
+++ tf-resources/s3.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "audio_artifacts_bucket" {
-  bucket = "${var.s3_bucket_name}"
+  bucket = var.s3_bucket_name
 }
